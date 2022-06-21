65b4038555f8ae9c38c7029474c670f380613edb
Andy <xtream77@gmail.com>
2022-06-22 01:17:57 +0900

    first commit

---
+++ src/test/java/com/xtmsoft/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtmsoft.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
